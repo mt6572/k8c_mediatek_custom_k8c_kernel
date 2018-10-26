ad35a517914d0ff337a2e3f9a72bd4ac39251809
sooorajjj <sooorajjj@gmail.com>
2018-10-26 23:09:10 +0530

    bmi160 Gyro + Accelrometer fixes

--- gyroscope/bmi160_gyro/cust_gyro.c
+++ gyroscope/bmi160_gyro/cust_gyro.c
@@ -3,27 +3,16 @@
 #include <cust_gyro.h>
 
 /*---------------------------------------------------------------------------*/
-int cust_gyro_power(struct gyro_hw *hw, unsigned int on, char* devname)
-{
-    if (hw->power_id == MT65XX_POWER_NONE)
-        return 0;
-    if (on)
-        return hwPowerOn(hw->power_id, hw->power_vol, devname);
-    else
-        return hwPowerDown(hw->power_id, devname); 
-}
-/*---------------------------------------------------------------------------*/
 static struct gyro_hw cust_gyro_hw = {
     .i2c_num = 1,
     .direction = 2,
     .power_id = MT65XX_POWER_NONE,  /*!< LDO is not used */
     .power_vol= VOL_DEFAULT,        /*!< LDO is not used */
-    .firlen = 16,                   /*!< don't enable low pass fileter */
-    .power = cust_gyro_power,
+    .firlen = 0,                   /*!< don't enable low pass fileter */
+    .is_batch_supported = false,
 };
 /*---------------------------------------------------------------------------*/
-struct gyro_hw* get_cust_gyro_hw(void) 
+struct gyro_hw* bmi160_get_cust_gyro_hw(void)
 {
     return &cust_gyro_hw;
 }
- 
